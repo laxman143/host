dbaa92f3a2b58ce440143e3490ced2e99dc0e692
laxman143 <laxmanprajapati143@gmail.com>
2025-06-01 18:10:45 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'microfrontend',
+    loadComponent:()=> loadRemoteModule('mfe1','./HomeComponent')
+    .then((m)=>m.HomeComponent)
+}];
